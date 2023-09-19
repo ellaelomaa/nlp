8e83b050ca02058b2545349a4c5954a3e3078d31
helmipls <115155909+helmipls@users.noreply.github.com>
2023-09-19 15:21:59 +0300

    I was nice today

--- NLP/fetch.py
+++ NLP/fetch.py
@@ -1,11 +1,29 @@
+import os
 
-# Hae_teksti txt/docs + määrittele filename
-def filename():
-     teksti = []
-     #from tekstit read jotainjotain
+# Define the path to the "tekstit" folder in your working directory
+folder_path = "NLP/corpora"
 
+# Get a list of all subfolders (corpora) in the "tekstit" folder
+corpora = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]
 
-# hae_korpus + määrittele corpusname
-def corpusname():
-     korpus = []
-     #from tekstit hae kaikki tai jotian read from folder jotain
+# Iterate through each corpus folder and read its content
+for corpus_name in corpora:
+    # Define the path to the current corpus folder
+    corpus_folder_path = os.path.join(folder_path, corpus_name)
+    
+    # Initialize a list to store the content of the corpus
+    corpus_content = []
+    
+    # Iterate through the files in the corpus folder
+    for filename in os.listdir(corpus_folder_path):
+        file_path = os.path.join(corpus_folder_path, filename)
+        
+        # Read the content of each file and append it to the corpus_content list
+        with open(file_path, 'r', encoding='utf-8') as file:
+            file_content = file.read()
+            corpus_content.append(file_content)
+    
+    # Now, you have the name of the corpus (corpus_name) and its content (corpus_content)
+    # You can process or analyze the content of each corpus as needed
+    print(f"Corpus Name: {corpus_name}")
+
