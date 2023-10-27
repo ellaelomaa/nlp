7fa6578f8edbf6b0dd5ab43e922b6b017dac8d98
Ella Elomaa <98178412+ellaelomaa@users.noreply.github.com>
2023-10-27 13:14:07 +0300

    Temp

--- NLP/clean.py
+++ NLP/clean.py
@@ -1,31 +1,41 @@
+import string
 import tokenisointi
-from uralicNLP import uralicApi
 
-# Normalisointi
-def normalisointi(tokenit):
-    normalisoitu = []
-    for word in tokenit:
-        normalisoitu = uralicApi.lemmatize(word, "fin")
-        if normalisoitu:
-            normalisoitu.append(normalisoitu[0]['lemma'])
-    return normalisoitu
+# poista tietyt sanat (erillinen lista)
 
-# Siivoa tekstistä tarvittavat asiat?? Tätä saa ja pitää muokata
-def siivoa():
-    siivottu_teksti = []
-    for erikoismerkit in korpus:
-        # Korvaa ja-merkit ja-sanalla
-        erikoismerkit = erikoismerkit.replace('&', 'ja')
-        # Poistaa hipsut
-        erikoismerkit = erikoismerkit.replace('"', '')
-        siivottu_teksti.append(erikoismerkit)
-    return siivottu_teksti
+# Funktiosanojen poisto
+def hukkasanojen_poisto(korpus, polku):
+
+    tiedosto = open(polku, "r")
+    data = tiedosto.read()
+    # Muutetaan tiedosto listaksi
+    funktiosanat = data.split("\n")
+
+    for avain in korpus:
+        sanalista = tokenisointi.tokenisoi_sanat(korpus[avain])
+        print("Ennen: ", len(sanalista))
+        temp = [sana for sana in sanalista if sana not in funktiosanat]
+        print("Jälkeen: ", len(temp))
+        korpus[avain] = temp
+
+    return korpus
 
 
-# poista tekstistä erisnimet (sanalista?)
-def poista_erisnimet(korpus):
-    erisnimet = []
-    with open('erisnimet.txt', 'r') as file:
-    erisnimet = file.read().splitlines()
-    korpus_ei_erisnimiä = [token for token in erisnimet if token not in erisnimet]
-    return korpus_ei_erisnimiä
+# Sisältösanojen poisto
+def sisaltosanojen_poisto(korpus, polku):
+    tiedosto = open(polku, "r")
+    data = tiedosto.read()
+    sisaltosanat = data.split("\n")
+    
+    for avain in korpus:
+        sanalista = ella_tokenisointi.tokenisoi_sanat(korpus[avain])
+        print("Ennen: ", len(sanalista))
+        temp = [sana for sana in sanalista if sana not in sisaltosanat]
+        print("Jälkeen: ", len(temp))
+        korpus[avain] = temp
+
+    return korpus
+
+def poistot(korpus, hukkabool, hukkapolku, sisaltobool, sisaltopolku):
+    if hukkabool == True:
+        hukkasanojen_poisto(korpus, hukkapolku)
