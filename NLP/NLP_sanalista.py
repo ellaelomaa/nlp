1c2aae6ba9590a7083ebda8ae5a3ec954b26be67
helmipls <115155909+helmipls@users.noreply.github.com>
2023-10-27 13:30:34 +0300

    cnzc

--- NLP/NLP_sanalista.py
+++ NLP/NLP_sanalista.py
@@ -1,10 +1,44 @@
+import clean.py
 
-# sanalistojen luominen ja vertailu
-# 30/50/100 HFL
-# funktiosanat
-# stopwordit
-# hapax legomena
-# frekvenssit (cosine similarity)
-# jaccardi + eucledian + jotain muuta?
+# Funktiosanalista
+def funktiosanat(normalisoitu):
+    funktiosanalista = []
+    with open("funktiosanat.txt", "r") as file:
+        funktiosanat_listassa = file.read().splitlines()
+    for funktiosana in normalisoitu:
+        if funktiosana in funktiosanat_listassa:
+            funktiosanalista.append(funktiosana)
+    return funktiosanalista
 
-# --> TÄHÄN KÄYTTÖLIITTYMÄ
+# Sisältösanalista 
+def sisältösanat(normalisoitu):
+    sisältösanalista = []
+    with open("funktiosanat.txt", "r") as file:
+        funktiosanat_listassa = file.read().splitlines()
+    for funktiosana in normalisoitu:
+        if funktiosana in funktiosanat_listassa:
+            sisältösanalista.append(funktiosana)
+    return sisältösanalista
+
+
+# High frequency lexicon 30/50/100 (lemmattu, funktiosanat poistettu) määrävalinta gui:hin
+def taajuussanasto(tokenit):
+    taajuussanalista = []
+    # poista erisnimet
+    # normalisointi
+    # poista funktiosanat
+    # freqdist
+    return taajuussanalista
+
+# Hapax legomena (sanat jotka esiintyy vaan kerran tai harvoin, lemmatuista ja ei lemmatuista listoista)
+def hapakslegomena(taajuussanalista):
+    # saisko tän niin että sort freqdist-lista vaa nousevasti ja siitä sitte?
+    return hapakslegomena
+
+# Sanaston laajuus (lemmatusta listasta ja remove duplicates jne eli uniikit sanat jne.)
+def saneet(tokenit):
+    sanelista = []
+    # poista erisnimet
+    # normalisointi
+    # remove duplicates
+    return sanelista
---
+++ NLP/gui.py
@@ -0,0 +1,128 @@
+import PySimpleGUI as sg
+import fetch
+import NLP.tokenisointi as tokenisointi
+import clean
+
+# Tässä ajetaan itse ohjelma asetusten mukaan läpi
+def kaynnista():
+    korpus = fetch.hae_korpus(asetukset["korpuspolku"])
+    tokenisointi.tokenisoi(korpus, asetukset["tokenisointi"])
+    clean.poistot(korpus, asetukset["hukkasanat"], asetukset["hukkasanapolku"])
+
+# Ehdotan, että asetuksia varten luodaan sanakirja.
+# Esim. jos mahdolliset asetukset ovat alustavasti lemmaus, stemmaus ja hukkasanat,
+# olisi sanakirjan default-arvoina False, False ja False. Asetuksia muuttamalla
+# saisi esimerkiksi listan False, True ja True. Tämä lista välitettäisiin
+# parametrina main-funktioon, tai missä ikinä päätetäänkään mitä
+# korpukselle/tekstille halutaan tehdä.
+
+# Sanakirjan alustus
+asetukset = {
+    "lemmaus": False,
+    "hukkasanat": False,
+    "funktiosanat": False,
+    "erisnimet": False,
+    "hukkasanapolku": "",
+    "sisaltosanapolku": "",
+    "erisnimipolku": "",
+    "korpuspolku": "",
+    "tokenisointi": "sanoiksi"
+}
+
+# PySimpleGuin eräs perusteemoista, saadaanpahan jotain söpöä hetkeksi :)
+sg.theme("LightGreen10")
+
+# Ulkoasu. Jokainen [ ] on yksi rivi. Jokaiseen asetukseen pitää laittaa
+# avain ja enable_events
+layout = [
+    [sg.Text("Asetukset", font=("Arial Bold", 20))],
+
+    # Käsiteltävien tekstien valinta
+    [
+        sg.Text("Valitse korpuksen sisältävä kansio:"),
+        sg.LBox([], size=(50, 1), expand_x=True,
+                expand_y=True, key="lista"),
+        sg.Input(visible=False, enable_events=True, key="korpuspolku",
+                 font=("Arial Bold", 10), expand_x=True),
+        sg.FolderBrowse()
+    ],
+
+    # Normalisointitavan valinta
+    [
+        sg.Radio("Perusmuotoista", "lemmaus",
+                 enable_events=True, key='lemmaa'),
+        sg.Radio('Ei mitään', "lemmaus", enable_events=True,
+                 key='eilemmaa', default=True)
+    ],
+
+    # Tokenisointitavan valinta
+
+    # Normalisointitavan valinta
+    [
+        sg.Text("Tokenisoi "),
+        sg.Radio("sanoiksi", "token",
+                 enable_events=True, key='sanoiksi', default=True),
+        sg.Radio('lauseiksi', "token", enable_events=True, key='lauseiksi'),
+        sg.Radio('virkkeiksi', "token", enable_events=True,
+                 key='virkkeiksi')
+    ],
+
+
+    # Poistettavien sanojen valinta
+    [sg.Checkbox(text=("Poista hukkasanat"), default=(
+        False), key="hukka", enable_events=True),
+        sg.Input(enable_events=True, key="hukkasanapolku"),
+        sg.FileBrowse()],
+    [sg.Checkbox(text=("Poista sisältösanat"), default=(
+        False), key="sisalto", enable_events=True),
+        sg.Input(enable_events=True, key="sisaltosanapolku"),
+        sg.FileBrowse()],
+    [sg.Checkbox(text=("Poista erisnimet"), default=(
+        False), key="erisnimet", enable_events=True),
+        sg.Input(enable_events=True, key="erisnimipolku"),
+        sg.FileBrowse()],
+
+    [sg.Button("Käynnistä ohjelma")],
+    [sg.Button("Sulje")]
+]
+
+# Luodaan ikkuna
+window = sg.Window("Helmin NLP-ohjelma",
+                   layout, )
+
+# Ikkuna on auki ja käyttäjä silmukassa, kunnes itse sulkee ohjelman
+while True:
+    event, values = window.read()
+    if event == "Sulje" or event == sg.WIN_CLOSED:
+        break
+
+    if event == "Käynnistä ohjelma":
+        kaynnista()
+
+    # Päivitetään laatikkoon valittujen tiedostojen polut
+    if event == "korpuspolku":
+        window["lista"].Update(values["korpuspolku"].split(";"))
+
+    # Päivitetään valintojen perusteella asetussanakirjaa
+    if values["sanoiksi"] == True:
+        asetukset["tokenisointi"] = "sanoiksi"
+
+    if values["lauseiksi"] == True:
+        asetukset["tokenisointi"] = "lauseiksi"
+        
+    if values["virkkeiksi"] == True:
+        asetukset["tokenisointi"] = "virkkeiksi"
+
+
+    asetukset["hukkasanat"] = values["hukka"]
+    asetukset["funktiosanat"] = values["sisalto"]
+    asetukset["erisnimet"] = values["erisnimet"]
+    asetukset["lemmaus"] = values["lemmaa"]
+
+    # Haetaan poistettavien sanalistojen polut
+    asetukset["hukkasanapolku"] = values["hukkasanapolku"]
+    asetukset["sisaltosanapolku"] = values["sisaltosanapolku"]
+    asetukset["erisnimipolku"] = values["erisnimipolku"]
+    asetukset["korpuspolku"] = values["korpuspolku"]
+
+window.close()
